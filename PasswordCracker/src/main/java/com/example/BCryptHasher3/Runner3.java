ccafc6d69cf5d961e7dbf616419d8525e8913ebd
JonnyHam <jonajonkim@gmail.com>
2022-11-04 17:16:19 -0500

    hi

--- PasswordCracker/src/main/java/com/example/BCryptHasher3/Runner3.java
+++
@@ -1,9 +0,0 @@
-package com.example.BCryptHasher3;
-
-public class Runner3 {
-    public static void main (String[] args) {
-        String pTest = "monkey123";
-        Bcrypt3 test = new Bcrypt3(pTest);
-        System.out.println(test.hasher());
-    }
-}
