7253fd16f0a9070f6015ad60edefd2ab09046d41
JonnyHam <jonajonkim@gmail.com>
2022-11-07 01:39:28 -0600

    hi

--- BCryptHasher3/src/main/java/com/example/BCryptHasher3/BcryptRunner3.java
+++ BCryptHasher3/src/main/java/com/example/BCryptHasher3/BcryptRunner3.java
@@ -1,6 +1,6 @@
 package com.example.BCryptHasher3;
 
-public class Runner3 {
+public class BcryptRunner3 {
     public static void main (String[] args) {
         String pTest = "monkey123";
         Bcrypt3 test = new Bcrypt3();
