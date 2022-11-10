c53695ece9b0b7a884af22c04d75a821b2fc35e7
JonnyHam <jonajonkim@gmail.com>
2022-11-10 10:34:35 -0600

    hi

--- BCryptHasher3/src/main/java/com/example/BCryptHasher3/PasswordChecker.java
+++ BCryptHasher3/src/main/java/com/example/BCryptHasher3/PasswordChecker.java
@@ -1,5 +1,28 @@
 package com.example.BCryptHasher3;
+import java.util.*;
 
 public class PasswordChecker {
+    public static void main (String[] args)  throws Exception {
+        PasswordReader test = new PasswordReader();
+        System.out.println(test.readPasswords());
+        String passwords = test.readPasswords();
+        String[] passwordList = passwords.split(" ");
+        System.out.println(Arrays.toString(passwordList));
+        System.out.println(passwordList[1000]);
+
+        String[] passwordsHashed = new String[passwordList.length];
+
+        MD5 test2 = new MD5();
+        String hashedPasswords = "";
+        for (int i = 0; i < passwordList.length; i++) {
+            String hashedPassword;
+            hashedPassword = test2.encryptString(passwordList[i], "MD5");
+            passwordsHashed[i] = hashedPassword;
+            hashedPasswords += hashedPassword + "\n";
+        }
+        //System.out.println(Arrays.toString(passwordsHashed));
+        System.out.println(hashedPasswords);
+    }
+
 
 }
--- BCryptHasher3/src/main/java/com/example/BCryptHasher3/PasswordReader.java
+++ BCryptHasher3/src/main/java/com/example/BCryptHasher3/PasswordReader.java
@@ -3,6 +3,30 @@ import java.io.*;
 
 
 public class PasswordReader {
+    private String passwords;
+    public String readPasswords () throws Exception {
+        passwords = "";
+        File file = new File(
+                "C:\\Users\\100117545\\Downloads\\BCryptHasher3\\BCryptHasher3\\src\\main\\java\\com\\example\\BCryptHasher3\\10KPass.txt");
+
+        // Note:  Double backquote is to avoid compiler
+        // interpret words
+        // like \test as \t (ie. as a escape sequence)
+
+        // Creating an object of BufferedReader class
+        BufferedReader br
+                = new BufferedReader(new FileReader(file));
+
+        // Declaring a string variable
+        String st;
+        // Condition holds true till
+        // there is character in a string
+        while ((st = br.readLine()) != null)
+            passwords += st + " ";
+            // Print the string
+            System.out.println(st);
+        return passwords;
+    }
     public static void main (String[] args) throws Exception {
         File file = new File(
                 "C:\\Users\\100117545\\Downloads\\BCryptHasher3\\BCryptHasher3\\src\\main\\java\\com\\example\\BCryptHasher3\\10KPass.txt");
