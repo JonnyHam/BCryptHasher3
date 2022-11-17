fb9093ba2a503093e698564486d816835f23d1cd
JonnyHam <jonajonkim@gmail.com>
2022-11-17 10:50:19 -0600

    still working on BruteForce attack. making it recursive. working on outputing correct numbers, but have correct amount of values in each arrayList iteration.

--- BCryptHasher3/src/main/java/com/example/BCryptHasher3/BruteForce.java
+++ BCryptHasher3/src/main/java/com/example/BCryptHasher3/BruteForce.java
@@ -9,6 +9,7 @@ public class BruteForce {
     private ArrayList<Integer> plain;
     private String plainString;
     private boolean cracked;
+    private boolean add;
     public BruteForce (String h, String t) {
         hash = h;
         type = t;
@@ -28,11 +29,38 @@ public class BruteForce {
             if (test.checkString(plainString, "MD5", hash) || test.checkString(plainString, "SHA-256", hash)) {
                 cracked = true;
             } else {
-
+                recursion(plain);
             }
         }
         return "";
     }
 
+    public ArrayList<Integer> recursion (ArrayList<Integer> arr) throws NoSuchAlgorithmException {
+        /*
+        String plainString2 = "";
+        for (int i = 0; i < arr.size(); i++) {
+            plainString2 += (char)((int)arr.get(i));
+        }
+         */
+        int x = arr.size()-1;
 
+        ArrayList<Integer> original= arr;
+        if (arr.size() > 1) {
+            arr.remove(0);
+            recursion(arr);
+        }
+        arr.add(0,original.get(0));
+        if (arr.get(0).equals(126) && add) {
+            if (arr.size() == plain.size()) {
+                arr.add(32);
+            }
+            arr.set(0, 32);
+            add = true;
+        }else if(add){
+            arr.set(0,arr.get(0)+1);
+        }
+        System.out.println(Arrays.deepToString(arr.toArray()));
+
+        return arr;
+    }
 }
--- BCryptHasher3/src/main/java/com/example/BCryptHasher3/RunnerMain.java
+++ BCryptHasher3/src/main/java/com/example/BCryptHasher3/RunnerMain.java
@@ -1,14 +1,24 @@
 package com.example.BCryptHasher3;
 
 import java.security.NoSuchAlgorithmException;
-
+import java.util.*;
 public class RunnerMain {
     public static void main (String[] args) throws Exception {
 
+        /*
         PasswordChecker test = new PasswordChecker("100117545");
         //MD5
         System.out.println(test.checkHash("df0349ce110b69f03b4def8012ae4970"));
         //SHA-256
         System.out.println(test.checkHash("82968e1da9cf580682e99ae94d78688d89684c0872e901c8b60cde135cd5369"));
+         */
+
+        BruteForce test = new BruteForce("df0349ce110b69f03b4def8012ae4970", "MD5");
+        ArrayList<Integer> arrTest = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            arrTest.add(i);
+        }
+        test.recursion(arrTest);
+
     }
 }
