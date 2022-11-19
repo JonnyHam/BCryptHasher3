6cc5272a8104979ea8f58bc6828df1896b6eea50
JonnyHam <jonajonkim@gmail.com>
2022-11-19 15:50:01 -0600

    Finished brute force attack for MD5 and SHA-256 in BruteForce2 class.

---
+++ BCryptHasher3/src/main/java/com/example/BCryptHasher3/BruteForce2.java
@@ -0,0 +1,46 @@
+package com.example.BCryptHasher3;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+public class BruteForce2 {
+    //private String type;
+    private String hash;
+    private ArrayList<Integer> plain;
+    private String plainString;
+    private boolean cracked;
+
+    public String attack (String h) throws NoSuchAlgorithmException {
+        hash = h;
+        cracked = false;
+        plain = new ArrayList<>();
+        plain.add(32);
+        plainString = "";
+
+        while (!cracked) {
+            plainString = "";
+            for (int i = 0; i < plain.size(); i++) {
+                plainString += (char)((int)(plain.get(i)));
+            }
+            SHAMD5 test = new SHAMD5();
+            if ((test.checkString(plainString, "MD5", hash)) || (test.checkString(plainString, "SHA-256", hash))) {
+                cracked = true;
+                return "Cracked hash: " + plainString;
+            } else {
+                for (int i = plain.size()-1; i > -1; i--) {
+                    if (plain.get(i) < 126) {
+                        int letter = plain.get(i)+1;
+                        plain.set(i, letter);
+                        break;
+                    }
+                    plain.set(i, 32);
+                    if (i == 0) {
+                        plain.add(32);
+                        break;
+                    }
+                }
+                //System.out.println(plainString);
+            }
+        }
+        return "end";
+    }
+}
--- BCryptHasher3/src/main/java/com/example/BCryptHasher3/RunnerMain.java
+++ BCryptHasher3/src/main/java/com/example/BCryptHasher3/RunnerMain.java
@@ -6,19 +6,44 @@ public class RunnerMain {
     public static void main (String[] args) throws Exception {
 
         /*
-        PasswordChecker test = new PasswordChecker("100117545");
+        PasswordChecker test = new PasswordChecker("Jonathan Kim");
         //MD5
         System.out.println(test.checkHash("df0349ce110b69f03b4def8012ae4970"));
         //SHA-256
         System.out.println(test.checkHash("82968e1da9cf580682e99ae94d78688d89684c0872e901c8b60cde135cd5369"));
          */
 
+        /*
         BruteForce test = new BruteForce("df0349ce110b69f03b4def8012ae4970", "MD5");
         ArrayList<Integer> arrTest = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             arrTest.add(i+118);
         }
         test.bruteForceAttack();
+        */
 
+
+        BruteForce2 test = new BruteForce2();
+        //System.out.println(test.attack("5f3f4681121b460e3304a1887f42f1c3"));
+        //System.out.println(test.attack("9145ad2b33c1c38967ddda4619d46f40"));
+        System.out.println(test.attack("e76a383d183d17223550bb1c29189dcf"));
+
+        /*
+        System.out.println((int)('a'));
+        System.out.println((int)('z'));
+
+        for (int i = (int)('a'); i <= (int)('z'); i++) {
+            System.out.println((char)(i));
+        }
+         */
+
+        /*
+        ArrayList<Integer> test = new ArrayList<>();
+        test.add((int)('a'));
+        System.out.println(Arrays.deepToString(test.toArray()));
+        int letter = test.get(0)+1;
+        test.set(0, letter);
+        System.out.println(Arrays.deepToString(test.toArray()));
+         */
     }
 }
